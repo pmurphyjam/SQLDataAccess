5bdd0288a750f55fc1e1d57f6e2a23000592127a
Pat Murphy <pat@docspera.com>
2017-03-06 00:38:25 -0800

    Updating

--- Example/SQLDataAccess/DAViewController.m
+++ Example/SQLDataAccess/DAViewController.m
@@ -7,17 +7,43 @@
 //
 
 #import "DAViewController.h"
+#import "AppManager.h"
 
 @interface DAViewController ()
+
+@property(nonatomic,weak) IBOutlet UITextView *textView;
+@property(nonatomic,weak) IBOutlet UILabel *dataLabel;
 
 @end
 
 @implementation DAViewController
 
+@synthesize textView;
+@synthesize dataLabel;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [dataLabel setText:@"Should see a simple SQLDataAccess here."];
+    
+    NSMutableArray *dataArray = [[AppManager SQLDataAccess] GetRecordsForQuery:@"select * from AppInfo ",nil];
+    NSLog(@"DAView : dataArray = %@",dataArray);
+    NSMutableString *data = [NSMutableString new];
+    if([dataArray count] > 0)
+    {
+        [data appendString:@"Database : Example.db \r"];
+        [data appendString:@"SQL : 'select * from AppInfo' \r"];
+        [data appendString:@"Results are : \r"];
+        [data appendFormat:@"%@",dataArray];
+        [textView setText:data];
+     }
+    
 }
 
 - (void)didReceiveMemoryWarning
